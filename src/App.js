1de7b2e0a16a15bc3dda755de1920aeee3be8b6f
Baz Murphy <bazmurphy@gmail.com>
2022-08-22 19:07:51 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Info from './Info';
 
 function App() {
+
+  const [quote, setQuote] = React.useState([]);
+
+  const [refresh, setRefresh] = React.useState(0)
+   
+  React.useEffect(() => {
+    fetch(`https://cyf-bazmurphy-quotes.herokuapp.com/quotes/random`)
+      .then(res => res.json())
+      .then(data => setQuote(data))
+      .catch(error => console.log(error));
+  }, [refresh]);
+
+  const getAnotherQuote = () => {
+
+    // fetch(`https://cyf-bazmurphy-quotes.herokuapp.com/quotes/random`)
+    // .then(res => res.json())
+    // .then(data => setQuote(data))
+    // .catch(error => console.log(error));
+
+    // ^ this is not DRY but i want to FETCH again onClick of the button
+
+    setRefresh(Math.random())
+    // ^so i came up with this sillyness to trigger a State Change, to trigger a refresh, to trigger a new fetch!
+    // console.log(refresh);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Info />
+      <div className="functionality-container">
+        <button className="functionality-button" onClick={getAnotherQuote}>Get Another Quote</button>
+      </div>
+      <div className="quote-container">
+        <p className="quote-text">"{quote.quote}"</p>
+        <p className="quote-author">- {quote.author}</p>
+      </div>
     </div>
   );
-}
+
+};
 
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Info = () => {
+    return (
+        <div className="info-container">
+            
+            <div className="info-back-end">
+            
+                <p className="info-back-end-title">Back End</p>
+
+                <div className="info-source-code">
+                    <span className="info-source-code-text">Source Code</span>
+                    <img className="info-source-code-image" src="github-dark.png" alt="github logo"></img>
+                    <a href="https://github.com/bazmurphy/node-quotes-server" target="_blank" rel="noreferrer">https://github.com/bazmurphy/node-quotes-server</a>
+                </div>
+                <div className="info-hosted">
+                    <span className="info-hosted-text">Hosted</span>
+                    <img className="info-hosted-image" src="heroku.png" alt="heroku logo"></img>
+                    <a href="https://cyf-bazmurphy-quotes.herokuapp.com/" target="_blank" rel="noreferrer">https://cyf-bazmurphy-quotes.herokuapp.com/</a>
+                </div>
+            </div>
+
+            <div className="info-front-end">
+
+                <p className="info-front-end-title">Front End</p>
+                
+                <div className="info-source-code">
+                    <span className="info-source-code-text">Source Code</span>
+                    <img className="info-source-code-image" src="github-dark.png" alt="github logo"></img>
+                    <a href="https://github.com/bazmurphy/node-quotes-server-react-front-end" target="_blank" rel="noreferrer">https://github.com/bazmurphy/react-quotes</a>
+                </div>
+                
+                <div className="info-hosted">
+                    <span className="info-hosted-text">Hosted</span>
+                    <img className="info-hosted-image" src="netlify.png" alt="netlify logo"></img>
+                    <a href="https://cyf-bazmurphy-quotes.netlify.app" target="_blank" rel="noreferrer">https://cyf-bazmurphy-quotes.netlify.app</a>
+                </div>
+                
+            </div>            
+        </div>
+    );
+};
+
+export default Info;
