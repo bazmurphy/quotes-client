ad6cb856243a6072f8b24b782421c4f53fa33150
Baz Murphy <bazmurphy@gmail.com>
2022-08-22 19:13:36 +0100

    fixed link error

--- src/Info.js
+++ src/Info.js
@@ -27,7 +27,7 @@ const Info = () => {
                 <div className="info-source-code">
                     <span className="info-source-code-text">Source Code</span>
                     <img className="info-source-code-image" src="github-dark.png" alt="github logo"></img>
-                    <a href="https://github.com/bazmurphy/node-quotes-server-react-front-end" target="_blank" rel="noreferrer">https://github.com/bazmurphy/react-quotes</a>
+                    <a href="https://github.com/bazmurphy/react-quotes" target="_blank" rel="noreferrer">https://github.com/bazmurphy/react-quotes</a>
                 </div>
                 
                 <div className="info-hosted">
